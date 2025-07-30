f100b80c1893415328ea60143f040429dac7fbe1
Ryan Luckie <rtluckie@gmail.com>
2025-07-29 21:09:31 -0500

    WIP 2025.07.29-210931

---
+++ config/definitions/includes.h
@@ -0,0 +1,5 @@
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/mouse.h>
+#include "zmk-helpers/helper.h"
